e174457b8a797636093112a8a9dde6c505e537cb
Jia Kim <120837059+jiakim2121@users.noreply.github.com>
2022-12-17 12:51:05 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+.container13 .heads {
+  opacity:.8;
+ 
+  width: 15%;
+  height: 15%; 
+  background-image: linear-gradient(#fff,#fff,#fff,#fff,#fff,#fff,#fff,#fff,#fff,#7caead);
+  transform: rotateZ(15deg);
+  border-radius: 50% 50% 40% 70% / 50% 50% 60% 50%; 
+   bottom:59%;
+  right:30%;  
+  
+}
